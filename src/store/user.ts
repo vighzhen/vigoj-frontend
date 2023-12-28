152c06a9fe88c1fa01cc359968834a4657f02afb
Vighzhen <728502339@qq.com>
2023-12-28 20:18:44 +0800

    前后联调

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,19 +1,30 @@
 // initial state
 import { StoreOptions } from "vuex";
 import ACCESS_ENUM from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
 
 export default {
   namespaced: true,
   state: () => ({
     loginUser: {
       userName: "未登录",
-      userRole: ACCESS_ENUM.NOT_LOGIN,
+      // userRole: ACCESS_ENUM.NOT_LOGIN,
     },
   }),
   actions: {
-    getLoginUser({ commit, state }, payload) {
+    async getLoginUser({ commit, state }, payload) {
+      // 远程请求获取登录信息
+      const res = await UserControllerService.getLoginUser();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: ACCESS_ENUM.NOT_LOGIN,
+        });
+      }
       // todo 改为从远程请求获取登录信息
-      commit("updateUser", payload);
+      // commit("updateUser", payload);
     },
   },
   mutations: {
